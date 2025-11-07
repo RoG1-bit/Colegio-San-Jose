94216bb21bfe513c85e1305fa33e2c4960907f5e
Roger Ramirez <rr251334@alumno.udb.edu.sv>
2025-11-06 23:08:55 -0600

    version 0.2

--- Colegio San Jose/Program.cs
+++ Colegio San Jose/Program.cs
@@ -1,3 +1,6 @@
+using Colegio_San_Jose.Data;
+using Microsoft.EntityFrameworkCore; // <-- Add this using directive
+
 namespace Colegio_San_Jose
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Colegio_San_Jose
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
